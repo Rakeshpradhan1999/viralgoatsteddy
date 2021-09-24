220e48bf1f36629a4ae70eb5cff557d91717660b
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-09-24 23:56:32 +0530

    changed config id

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ const App = () => {
     // you are connected to main net
     // Please connect to main net
 
-    if (chainId === 4) {
+    if (chainId === 1) {
       toast(`You are connected to main net`, {
         type: "success",
         position: toast.POSITION.BOTTOM_CENTER,
@@ -127,7 +127,7 @@ const App = () => {
 
   async function mint(mintCount) {
     if (contract) {
-      if (chainId === 4) {
+      if (chainId === 1) {
         if (mintCount === 0) {
           setLessMintAmountAlert(true);
         } else {
